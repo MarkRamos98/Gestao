bf20290ff5760848cec44cda0f80020470b854b9
MarkRamos98 <marcos.ramos123@live.com>
2024-11-21 20:14:48 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/Produtos_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Produtos_Activity.java
@@ -1,14 +1,25 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Produtos_Activity extends AppCompatActivity {
+
+    RecyclerView recycleprodutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +32,23 @@ public class Produtos_Activity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private List<Produtos> lsprodutos;
+
+    public View onCreate(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+
+        View view = inflater.inflate(R.layout.activity_produtos, container, false);
+
+        recycleprodutos = view.findViewById(R.id.recycleprodutos);
+
+        lsprodutos = new ArrayList<>();
+        lsprodutos.add(new Produtos("AAA",R.drawable.porcat,"AAA","AAA","AAA","AAA"));
+
+        Adapter adapter = new Produtos(getApplicationContext(),lsprodutos);
+
+        recycleprodutos.setLayoutManager(new GridLayoutManager(getApplicationContext(),1));
+
+        recycleprodutos.setAdapter(adapter);
+
+
 }
